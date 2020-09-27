667db07f682ca6ba51ca41a0f8ce295cf41dc37c
Anelisi <anelisilanmeller@gmail.com>
2020-09-27 17:14:48 -0300

    Nivelamento em Hibernate

---
+++ src/main/java/aplication/Programa.java
@@ -0,0 +1,34 @@
+package aplication;
+
+import dominio.Pessoa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Programa {
+    public static void main(String[] args) {
+
+//        Pessoa p1 = new Pessoa(null, "Anelisi Meller", "anelisi@unisinos.com");
+//        Pessoa p2 = new Pessoa(null, "Arthur Svensson", "arthur@unisinos.com");
+//        Pessoa p3 = new Pessoa(null, "Licodêncio Meller Svensson", "lico@etbilu.com");
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("java_coffee-jpa");
+        EntityManager em = emf.createEntityManager();
+
+//        em.getTransaction().begin();
+//        em.persist(p1);
+//        em.persist(p2);
+//        em.persist(p3);
+//
+//        em.getTransaction().commit();
+
+        Pessoa p = em.find(Pessoa.class, 3);
+        em.getTransaction().begin();
+        em.remove(p);
+        em.getTransaction().commit();
+
+        System.out.println("Pronto!");
+
+    }
+}
