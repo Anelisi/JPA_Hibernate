98ad2fdf20fe44a15acb8279d3a19fd79469b86f
Anelisi <anelisilanmeller@gmail.com>
2020-12-11 15:14:06 -0300

    Testando

--- src/main/java/aplication/Programa.java
+++ src/main/java/aplication/Programa.java
@@ -9,26 +9,29 @@ import javax.persistence.Persistence;
 public class Programa {
     public static void main(String[] args) {
 
-//        Pessoa p1 = new Pessoa(null, "Anelisi Meller", "anelisi@unisinos.com");
-//        Pessoa p2 = new Pessoa(null, "Arthur Svensson", "arthur@unisinos.com");
-//        Pessoa p3 = new Pessoa(null, "Licodêncio Meller Svensson", "lico@etbilu.com");
+        /* Pessoa p1 = new Pessoa(null, "Anelisi Meller", "anelisi@unisinos.com");
+         Pessoa p2 = new Pessoa(null, "Arthur Svensson", "arthur@unisinos.com");
+         Pessoa p3 = new Pessoa(null, "Licodêncio Meller Svensson", "lico@etbilu.com");
+        Pessoa p4 = new Pessoa(null,"Gretchen","congalaconga@piripiripirim.bum");*/
+
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("java_coffee-jpa");
         EntityManager em = emf.createEntityManager();
 
-//        em.getTransaction().begin();
-//        em.persist(p1);
-//        em.persist(p2);
-//        em.persist(p3);
-//
-//        em.getTransaction().commit();
+       /*em.getTransaction().begin();
+        em.persist(p4);
+        em.persist(p1);
+         em.persist(p2);
+         em.persist(p3);
 
-        Pessoa p = em.find(Pessoa.class, 3);
+         em.getTransaction().commit();*/
+
+        Pessoa p = em.find(Pessoa.class, 4);
+        System.out.println(p);
         em.getTransaction().begin();
         em.remove(p);
         em.getTransaction().commit();
 
         System.out.println("Pronto!");
-
     }
 }
--- src/main/java/dominio/Pessoa.java
+++ src/main/java/dominio/Pessoa.java
@@ -4,11 +4,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.io.Serializable;
 
 @Entity
-public class Pessoa implements Serializable {
-    private static final long serialVersionUID = 1L;
+public class Pessoa {
+
 @Id
 @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Integer id;
